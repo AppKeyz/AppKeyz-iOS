1789dd55dcd12704595b27593cb7c15b6eac0385
Michael Hayes <tasslehawf@mac.com>
2013-04-25 22:40:33 -0500

    Initial Commit

---
+++ AppKeyz Sample Application/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AppKeyz Sample Application
+//
+//  Created by Michael Hayes on 4/25/13.
+//  Copyright (c) 2013 App Keyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AppKeyz Sample Application/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppKeyz Sample Application
+//
+//  Created by Michael Hayes on 4/25/13.
+//  Copyright (c) 2013 App Keyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
